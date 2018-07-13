c22dffd3f4fc192ecd8978e3f675dfb90db696ec
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-07-12 22:01:36 -0700

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
+
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
@@ -60,7 +62,42 @@ app.delete('/todos/:id', (req,res) => {
     .catch(e => res.status(404).send())
 });
 
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // check completed val, use to se completedAt to timestamp
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, {new: true})
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+
+      res.send({todo});
+    })
+    .catch((e) => {
+      res.status(400).send(e)
+    });
+
+});
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Started up ${PORT}`));
 
 module.exports = { app };
+
+
+
+
